31e20444f09125bb831cc0b0f0ef46102e90febc
Mustafa DUMAN <115396551+u-MuDum@users.noreply.github.com>
2023-07-25 11:54:28 +0300

    BMI

--- 01-Fundamentals-Part-1/starter/app.js
+++ 01-Fundamentals-Part-1/starter/app.js
@@ -1,7 +1,29 @@
-let country = "Türkiye";
-let population = 85000000;
-let continent = "Asia";
+// let country = "Türkiye";
+// let population = 85000000;
+// let continent = "Asia";
 
-console.log(population);
-console.log(country);
-console.log(continent);
+// console.log(population);
+// console.log(country);
+// console.log(continent);
+
+//CHALLENGE #1
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = Math.floor(massMark / heightMark ** 2);
+const BMIJohn = Math.floor(massJohn / heightJohn ** 2);
+
+console.log(BMIMark, BMIJohn);
+
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(markHigherBMI);
+
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI is (${BMIMark}) higher than John's! (${BMIJohn})`);
+} else {
+  console.log(`John's BMI is (${BMIJohn}) higher than Mark's! (${BMIMark})`);
+}
