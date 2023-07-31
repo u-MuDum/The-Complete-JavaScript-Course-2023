bab9b6dc2c05ebb20bec0f3cf7e0bd40244002e9
Mustafa DUMAN <115396551+u-MuDum@users.noreply.github.com>
2023-07-31 17:38:57 +0300

    derse devam

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -25,18 +25,26 @@
 // const appleOrangeJuice = fruitProcessor(2, 4);
 // console.log(appleOrangeJuice);
 
-function yasHesapla1(dogumYili) {
-  let yas = 2037 - dogumYili;
-  return yas;
-}
+// function yasHesapla1(dogumYili) {
+//   let yas = 2037 - dogumYili;
+//   return yas;
+// }
 
-let yas1 = yasHesapla1(1991);
-console.log(yas1);
+// let yas1 = yasHesapla1(1991);
+// console.log(yas1);
 
-let yasHesapla2 = function (dogumYili) {
-  let yas = 2037 - dogumYili;
-  return yas;
+// let yasHesapla2 = function (dogumYili) {
+//   let yas = 2037 - dogumYili;
+//   return yas;
+// };
+
+// let yas2 = yasHesapla2(1985);
+// console.log(yas2);
+
+const emekliligeKalan = (dogumYili, adi) => {
+  const yas = 2045 - dogumyili;
+  const kalan = 65 - yas;
+  return `${adi} Bey, emekliliğinize ${kalan} yıl kaldı.`;
 };
 
-let yas2 = yasHesapla2(1985);
-console.log(yas2);
+console.log(emekliligeKalan(1990, "Mustafa"));
