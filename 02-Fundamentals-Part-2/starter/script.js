f37960e26f59b47435bb26fca1bb54017567f087
Mustafa DUMAN <115396551+u-MuDum@users.noreply.github.com>
2023-08-02 21:56:16 +0300

    functions dersi

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -25,26 +25,64 @@
 // const appleOrangeJuice = fruitProcessor(2, 4);
 // console.log(appleOrangeJuice);
 
-// function yasHesapla1(dogumYili) {
-//   let yas = 2037 - dogumYili;
+//UZUN FONKSİYON YAZIMI
+
+// function yasHesapla(dogumYili) {
+//   let yas = 2023 - dogumYili;
 //   return yas;
 // }
 
-// let yas1 = yasHesapla1(1991);
+// let yas1 = yasHesapla(1985);
 // console.log(yas1);
-
-// let yasHesapla2 = function (dogumYili) {
-//   let yas = 2037 - dogumYili;
-//   return yas;
-// };
-
-// let yas2 = yasHesapla2(1985);
+// let yas2 = yasHesapla(1979);
 // console.log(yas2);
 
-const emekliligeKalan = (dogumYili, adi) => {
-  const yas = 2045 - dogumyili;
-  const kalan = 65 - yas;
-  return `${adi} Bey, emekliliğinize ${kalan} yıl kaldı.`;
-};
+//KISA FONKSİYON YAZIMI
 
-console.log(emekliligeKalan(1990, "Mustafa"));
+// const yashesap1 = (yil1) => 2023 - yil1;
+// const yas2 = yashesap1(1985);
+// const yas3 = yashesap1(1979);
+// console.log(yas2);
+// console.log(yas3);
+
+//UZUN EMEKLİLİK FONKSİYON YAZIMI
+
+// const emekliligeKalan = function yasHesapla(dogumYili, adi) {
+//   const yas = 2023 - dogumYili;
+//   const kalan = 60 - yas;
+//   return `${adi} Bey, ${yas} yaşındasınız ve emekliliğinize ${kalan} yıl kaldı.`;
+// };
+
+// console.log(emekliligeKalan(1985, "Mustafa"));
+// console.log(emekliligeKalan(1979, "Kerim"));
+
+//KISA EMEKLİLİK FONKSİYON YAZIMI
+
+// const emekliligeKalan = (dogumYili, adi) => {
+//   const yas = 2023 - dogumYili;
+//   const kalan = 60 - yas;
+
+//   return `Sayın ${adi} Bey, yaşınız ${yas} olup emekliliğe kalan süreniz ${kalan} yıldır. İyi günler !`;
+// };
+
+// console.log(emekliligeKalan(1985, "Mustafa"));
+// console.log(emekliligeKalan(1979, "Kerim"));
+
+//KULLANICIDAN VERİ GİRİŞİ İLE KISA EMEKLİLİK FONKSİYON YAZIMI !!!!!!!!!!!!(GELİŞTİRİLMESİ LAZIM)!!!!!!!!!!!!!!!
+
+// const dogumYili = prompt("Lütfen doğum yılınızı giriniz");
+// const adi = prompt("Lütfen adınızı giriniz");
+// const emekliligeKalan = (dogumYili, adi) => {
+//   const yas = 2023 - dogumYili;
+//   const kalan = 60 - yas;
+
+//   return `Sayın ${adi} Bey, yaşınız ${yas} olup emekliliğe kalan süreniz ${kalan} yıldır. İyi günler !`;
+// };
+
+// alert(emekliligeKalan(dogumYili, adi));
+
+// const dogumYili2 = prompt("Lütfen doğum yılınızı giriniz");
+// const adi2 = prompt("Lütfen adınızı giriniz");
+// alert(emekliligeKalan(dogumYili2, adi2));
+
+// FUNCTION İÇİNDE FUNCTION
