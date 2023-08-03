334245f711923a7393c03c883710838430685469
Mustafa DUMAN <115396551+u-MuDum@users.noreply.github.com>
2023-08-03 11:03:25 +0300

    Challenge 6

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -89,24 +89,65 @@
 
 // CHALLENGE #5
 
-const ortHesapla = (sayi1, sayi2, sayi3) => {
-  const ort1 = (sayi1 + sayi2 + sayi3) / 3;
-  return ort1;
-};
-const scoreDolphins = ortHesapla(96, 108, 89);
+// const ortHesapla = (sayi1, sayi2, sayi3) => {
+//   const ort1 = (sayi1 + sayi2 + sayi3) / 3;
+//   return ort1;
+// };
+// const scoreDolphins = ortHesapla(96, 108, 89);
 
-const scoreKoalas = ortHesapla(88, 91, 110);
+// const scoreKoalas = ortHesapla(88, 91, 110);
 
-if (scoreDolphins > scoreKoalas) {
-  console.log(
-    `Dolphins win the trophy. (${Math.floor(scoreDolphins)} vs ${Math.floor(
-      scoreKoalas
-    )})`
-  );
-} else if (scoreDolphins < scoreKoalas) {
-  console.log(
-    `Koalas win the trophy. (${Math.floor(scoreDolphins)} vs ${Math.floor(
-      scoreKoalas
-    )})`
-  );
-} else console.log("Both win the trophy");
+// if (scoreDolphins > scoreKoalas) {
+//   console.log(
+//     `Dolphins win the trophy. (${Math.floor(scoreDolphins)} vs ${Math.floor(
+//       scoreKoalas
+//     )})`
+//   );
+// } else if (scoreDolphins < scoreKoalas) {
+//   console.log(
+//     `Koalas win the trophy. (${Math.floor(scoreDolphins)} vs ${Math.floor(
+//       scoreKoalas
+//     )})`
+//   );
+// } else console.log("Both win the trophy");
+
+// CHALLENGE #6 UZUN
+
+// function tipHesapla(bill) {
+//   if (bill < 300 && bill > 50) {
+//     let tip = bill * 0.15;
+//     return tip;
+//   } else {
+//     let tip = bill * 0.2;
+//     return tip;
+//   }
+// }
+
+// const bills = [125, 555, 44];
+
+// let tip1 = tipHesapla(bills[0]);
+// let tip2 = tipHesapla(bills[1]);
+// let tip3 = tipHesapla(bills[2]);
+// console.log(tip1, tip2, tip3);
+
+// const tips = [];
+// tips.push(tip1, tip2, tip3);
+
+// //tips.push(tip1, tip2, tip3, ...bills);  array içine array ekleme
+// console.log(tips);
+
+// const total = [tip1 + bills[0], tip2 + bills[1], tip3 + bills[2]];
+// console.log(total);
+
+// CHALLENGE #6 KISA
+
+// const calcTip = function (bill) {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// };
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+// console.log(bills, tips);
+
+// const total = [tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2]];
+// console.log(total);
