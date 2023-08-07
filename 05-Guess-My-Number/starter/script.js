b880fdee32a3aefa952d72b1bf9c66ad5919e932
Mustafa DUMAN <115396551+u-MuDum@users.noreply.github.com>
2023-08-07 11:25:12 +0300

    Guess My Number

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,28 @@
-'use strict';
+"use strict";
+
+/*console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = " 👍 Correct Number !";
+document.querySelector(".score").textContent = 50;
+console.log((document.querySelector(".guess").value = 26));
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector(".number").textContent = secretNumber;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector(".message").textContent = "No number";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = " 👍 Correct Number !";
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "📉 Too low !";
+    score--;
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "📈 Too high !";
+    score--;
+  }
+});
